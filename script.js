3724b92148a27c21cfeceabb132e7aabc52929f4
Divyanshu Kannaujiya <divyanshukannau143@outlook.com>
2025-06-02 23:29:30 +0530

    I have added a section to Display My Project

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
     }
   });
 });
+
+function projecttype(ptype){
+  const collab=document.getElementById("collab");
+ const indi=document.getElementById("individual");
+ const ongoing=document.getElementById("ongoing");
+ const signupForm=document.getElementById("signupform");
+  if(ptype==="collab"){
+     indi.classList.remove('active');
+    ongoing.classList.remove('active');
+    collab.classList.add('active');
+  
+  }
+  else if(ptype==="indi"){
+     ongoing.classList.remove('active');
+    collab.classList.remove('active');
+    indi.classList.add('active');
+   
+    
+  }
+   else{
+      indi.classList.remove('active');
+    collab.classList.remove('active');
+    ongoing.classList.add('active');
+    
+  }
+}
